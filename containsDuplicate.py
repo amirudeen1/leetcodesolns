b3fca822e061b435b8fb82d27b3dd218a76ba8a0
amirudeen1 <151761037+amirudeen1@users.noreply.github.com>
2024-07-08 22:18:36 +0800

    Create containsDuplicate.py

---
+++ containsDuplicate.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        hash = set()
+
+        for num in nums:
+            if num not in hash:
+                hash.add(num)
+            else:
+                return True
+        return False
